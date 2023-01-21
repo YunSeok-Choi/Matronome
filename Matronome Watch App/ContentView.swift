656c18080116101514c381620ef992dcf9d116a2
YunSeok-Choi <dbstjr9898@naver.com>
2023-01-21 17:22:36 +0900

    :tada: New project

---
+++ Matronome Watch App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Matronome Watch App
+//
+//  Created by 최윤석 on 2023/01/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Matronome Watch App/MatronomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatronomeApp.swift
+//  Matronome Watch App
+//
+//  Created by 최윤석 on 2023/01/21.
+//
+
+import SwiftUI
+
+@main
+struct Matronome_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
