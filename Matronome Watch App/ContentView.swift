00dfb725db560e41f0967519ed7bf3626994e7a3
YunSeok-Choi <dbstjr9898@naver.com>
2023-01-21 17:32:49 +0900

    :lipstick: 메트로놈 slider UI추가

--- Matronome Watch App/ContentView.swift
+++ Matronome Watch App/ContentView.swift
@@ -10,14 +10,34 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Slider()
+                .stroke()
+            
         }
         .padding()
     }
 }
+
+
+struct Slider: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        path.move(to: CGPoint(x: 20, y: 30))
+        
+        path.addCurve(to: CGPoint(x: 90, y: 110),
+                      control1: CGPoint(x: -10, y: 60),
+                      control2: CGPoint(x: 50, y: 130))
+        
+        path.addCurve(to: CGPoint(x: 20, y: 30),
+                      control1: CGPoint(x: 130, y: 85),
+                      control2: CGPoint(x: 60, y: 0))
+        
+        return path
+    }
+}
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
