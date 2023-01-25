3167d407dd08bd397cd71fda27689a8b1f2bdc12
YunSeok-Choi <dbstjr9898@naver.com>
2023-01-25 11:55:09 +0900

    :lipstick: 반전 Slider UI추가

--- Matronome Watch App/ContentView.swift
+++ Matronome Watch App/ContentView.swift
@@ -8,19 +8,52 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var radian = 6.25
+    
     var body: some View {
         VStack {
-            Slider()
-                .stroke()
-            
+            ZStack {
+                Slider()
+                    .stroke()
+                    .padding()
+                    .rotationEffect(.radians(radian), anchor: .bottom)
+                    .animation(.easeInOut(duration: 1).repeatForever(), value: radian)
+                
+                // 우 180 == 8.65
+                // 90 == 7.05
+                // 좌 180 == 5.55
+                
+                Slider()
+                    .stroke()
+                    .padding()
+                    .rotation3DEffect(.degrees(180), axis: (x: 0, y: 1, z: 0))
+                    .animation(.easeInOut(duration: 1).repeatForever(), value: radian)
+                
+            }
+            .onAppear{
+//                rotationEllipse()
+            }
+            .frame(width: 200, height: 150)
+//            .background(.blue)
         }
         .padding()
     }
+    
+    func rotationEllipse() {
+        
+        if radian < 100 {
+            radian -= 10
+        } else if radian > 0{
+            radian += 5
+        }
+    }
+    
 }
 
 
 struct Slider: Shape {
     func path(in rect: CGRect) -> Path {
+        
         var path = Path()
         
         path.move(to: CGPoint(x: 20, y: 30))
@@ -36,8 +69,6 @@ struct Slider: Shape {
         return path
     }
 }
-
-
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
