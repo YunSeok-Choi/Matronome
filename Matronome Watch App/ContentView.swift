d2504e2c9d53130e1bb327d30fce0e9c8fdeb054
YunSeok-Choi <dbstjr9898@naver.com>
2023-01-30 16:31:10 +0900

    :lipstick: bpm, setting UI 추가

--- Matronome Watch App/ContentView.swift
+++ Matronome Watch App/ContentView.swift
@@ -8,63 +8,107 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var radian = 6.25
+    @State var degree = 150.0
+    @State var bpm = 60
+    var angle = 210.0
+    
     
     var body: some View {
         VStack {
+            HStack {
+                Spacer()
+                
+                Label("", systemImage: "ellipsis")
+            }
+            .padding()
+            .padding(.top, 15)
+            
             ZStack {
-                Slider()
-                    .stroke()
-                    .padding()
-                    .rotationEffect(.radians(radian), anchor: .bottom)
-                    .animation(.easeInOut(duration: 1).repeatForever(), value: radian)
                 
-                // 우 180 == 8.65
-                // 90 == 7.05
-                // 좌 180 == 5.55
+                //MARK: 파랑
+                Ellipse()
+                    .fill(
+                        LinearGradient(gradient: Gradient(colors: [.blue, .clear]),
+                                       startPoint: .bottom, endPoint: .top)
+                    )
+                    .frame(width: 90, height: 130)
+                    .rotationEffect(.degrees(degree), anchor: .top)
+                    .position(x: 50, y: 180)
+                    .onAppear {
+                        withAnimation(.easeInOut(duration: 1)
+                            .speed(1)
+                            .repeatForever()) {
+                                degree = angle
+                            }
+                    }
                 
-                Slider()
-                    .stroke()
-                    .padding()
-                    .rotation3DEffect(.degrees(180), axis: (x: 0, y: 1, z: 0))
-                    .animation(.easeInOut(duration: 1).repeatForever(), value: radian)
+                //MARK: 초록
+                Ellipse()
+                    .fill(
+                        LinearGradient(gradient: Gradient(colors: [.green, .clear]),
+                                       startPoint: .top, endPoint: .bottom)
+                    )
+                    .frame(width: 90, height: 130)
+                    .rotationEffect(.degrees(degree), anchor: .bottom)
+                    .rotation3DEffect(.degrees(180), axis: (x: 1, y: 0, z: 0))
+                    .position(x: 50, y: 180)
+                    .onAppear {
+                        withAnimation(.easeInOut(duration: 1)
+                            .speed(1)
+                            .repeatForever()) {
+                                degree = angle
+                            }
+                    }
                 
             }
-            .onAppear{
-//                rotationEllipse()
-            }
-            .frame(width: 200, height: 150)
-//            .background(.blue)
+            .frame(width: 100, height: 100)
+            .padding()
+            
+            Text("\(bpm)")
+                .font(.system(size: 28))
+                .fontWeight(.light)
+    
+            
         }
         .padding()
     }
     
-    func rotationEllipse() {
+    func rotationLeftEllipse() {
         
-        if radian < 100 {
-            radian -= 10
-        } else if radian > 0{
-            radian += 5
+        if degree <= 180.0 {
+            degree -= 1
+        } else if degree >= 360.0 {
+            degree += 1
+        }
+        
+    }
+    
+    func rotationRightEllipse() {
+        
+        if degree <= 0.5 {
+            degree -= 1
+        } else if degree >= 0.1 {
+            degree += 1
         }
     }
     
 }
 
-
+//MARK: Ellipse
 struct Slider: Shape {
     func path(in rect: CGRect) -> Path {
         
         var path = Path()
         
-        path.move(to: CGPoint(x: 20, y: 30))
+        path.move(to: CGPoint(x: 0, y: 10))
         
         path.addCurve(to: CGPoint(x: 90, y: 110),
-                      control1: CGPoint(x: -10, y: 60),
-                      control2: CGPoint(x: 50, y: 130))
+                      control1: CGPoint(x: -40, y: 50),
+                      control2: CGPoint(x: 20, y: 120))
         
-        path.addCurve(to: CGPoint(x: 20, y: 30),
-                      control1: CGPoint(x: 130, y: 85),
-                      control2: CGPoint(x: 60, y: 0))
+        path.addCurve(to: CGPoint(x: 0, y: 10),
+                      control1: CGPoint(x: 140, y: 60),
+                      control2: CGPoint(x: 40, y: -25))
         
         return path
     }
